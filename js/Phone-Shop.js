3be3bb9c064fbefcde6b80d3aa86d0532c89cf7c
abdullahboshir <abdullahboshir@gmao.com>
2022-04-13 23:52:11 +0600

    nineth commit

--- js/Phone-Shop.js
+++ js/Phone-Shop.js
@@ -1,49 +1,52 @@
-// spinner 
+
+////////////// Spinner Loading //////////////
 const toggleSpinner = spinner => {
-   document.getElementById("spinner").style.display = spinner;
+     document.getElementById("spinner").style.display = spinner;
 }
 
-// blank input warning
+////////////// blank input warning //////////////
 const blankInput = warning => {
-  document.getElementById("blank-input").style.display = warning;
+     document.getElementById("blank-input").style.display = warning;
 }
 
-// incorrect items input warning
+/////////////// incorrect items input warning //////////////
 const incorrectInput = CorrectMsg => {
-  document.getElementById("incorrect-input").style.display = CorrectMsg;
+     document.getElementById("incorrect-input").style.display = CorrectMsg;
 }
 
 
-
+/////////////// Load Phone data //////////////
 const phoneDataLoad = () => {
-    const searchBox = document.getElementById("search-box").value;
+  const searchBox = document.getElementById("search-box").value;
   document.getElementById("search-box").value = "";
-  toggleSpinner("block");
-
+  
+  const cardContainer = document.getElementById("cards-container");
+    cardContainer.textContent = "";
+  
+    //  clear single cards container data
   const cardsContainerrow = document.getElementById("row-container");
-  cardsContainerrow.textContent = "";  
+     cardsContainerrow.textContent = ""; 
+  toggleSpinner("block");
+  incorrectInput("none")
   
   if (searchBox == "") {
-    blankInput("block");
-    toggleSpinner("none");
+     blankInput("block");
+     toggleSpinner("none");
   }
   else {
-    //    data Load 
-    blankInput("none");
-    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchBox}`)
+     blankInput("none");
+     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchBox}`)
         .then(res => res.json())
         .then(data => displayResult(data.data));
   }
 };
 
 
-
+/////////////// Display Phone items //////////////
 const displayResult = (phone) => {
-  // console.log(phone)
-  const cardContainer = document.getElementById("cards-container");
-  incorrectInput("none")
-  cardContainer.textContent = "";
-  toggleSpinner("none");
+ const cardContainer = document.getElementById("cards-container");
+    cardContainer.textContent = "";
+    toggleSpinner("none");
 
   if (phone.length == 0) {
     toggleSpinner("none")
@@ -71,21 +74,22 @@ const displayResult = (phone) => {
 };
 
 
+/////////////// Load single Phone details //////////////
 const phoneDetails = async phoneDetails => {
-    const phoneDetailsUrl = `https://openapi.programming-hero.com/api/phone/${phoneDetails}`;
-    const res = await fetch(phoneDetailsUrl);
-    const data = await res.json();
-  displaySinglePhone(data)
-  console.log(data)
+  const phoneDetailsUrl = `https://openapi.programming-hero.com/api/phone/${phoneDetails}`;
+  const res = await fetch(phoneDetailsUrl);
+  const data = await res.json();
+        displaySinglePhone(data);
 };
 
 
+/////////////// Display single Phone items //////////////
 const displaySinglePhone = singlePhones => {
     const cardsContainerrow = document.getElementById("row-container");
     cardsContainerrow.textContent = "";
     const div = document.createElement("div");
     div.classList.add("row");
-  div.innerHTML = `
+    div.innerHTML = `
     <div class="col-md-4";>
     <img style = "width: 940px" src="${singlePhones.data.image}" class="img-fluid rounded-start" alt="...">
   </div>
@@ -107,10 +111,9 @@ const displaySinglePhone = singlePhones => {
       <p class="card-text mb-1"><span class = "fw-bold">USB:</span> ${singlePhones.data.others.USB}</p>
       <p class="card-text mb-1"><span class = "fw-bold">NFC:</span> ${singlePhones.data.others.NFC}</p>
       <p class="card-text mb-1"><span class = "fw-bold">Radio:</span> ${singlePhones.data.others.Radio}</p>
-      <p class="card-text"><small class="text-muted">${singlePhones.data.releaseDate}</small></p>
+      <p class="card-text"><small class="text-muted">${singlePhones.data.releaseDate ? singlePhones.data.releaseDate: "no available release date"}</small></p>
     </div>
   </div>
- 
     `
     cardsContainerrow.appendChild(div)
 }
